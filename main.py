d9d7ee227f1459ebbf4ec1217759d6e361e014f0
4entertainment <58992739+4entertainment@users.noreply.github.com>
2023-09-22 10:35:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,53 @@
+from art import logo
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
             'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
-
-# TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar().
 def caesar(plain_text, shift_amount, direction):
     if direction == "encode":
         cipher_text = ""
         for letter in plain_text:
-            position = alphabet.index(letter)
-            new_position = position + shift_amount
-            cipher_text += alphabet[new_position]
+            if letter in alphabet:
+                position = alphabet.index(letter)
+                new_position = position + shift_amount
+                cipher_text += alphabet[new_position]
+            else:
+                cipher_text += letter
         print(f"The encoded text is {cipher_text}")
 
     elif direction == "decode":
         decoded_text = ""
         for letter in plain_text:
-            position = alphabet.index(letter)
-            new_position = position - shift_amount
-            decoded_text += alphabet[new_position]
+            if letter in alphabet:
+                position = alphabet.index(letter)
+                new_position = position - shift_amount
+                decoded_text += alphabet[new_position]
+            else:
+                decoded_text += letter
         print(f"The decoded text is {decoded_text}")
 
 
-# TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
-caesar(plain_text=text, shift_amount=shift, direction=direction)
-#ssl
+print(logo)
+
+
+should_end = False
+while not should_end:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = (int(input("Type the shift number:\n"))) % 26
+
+    caesar(plain_text=text, shift_amount=shift, direction=direction)
+
+
+    choice = input("If you want to continue type 'yes'; otherwise type 'no'\n")
+
+    if choice == "no":
+        print("Goodbye")
+        should_end = True
+    elif choice == "yes":
+        continue
+    else:
+        print("Please type correctly...")
+
